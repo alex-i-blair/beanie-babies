de6d974215719699c2d49b15435cc910b31eb04c
Alex Blair <bclan11@gmail.com>
2022-02-07 15:58:53 -0800

    searching works

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -6,19 +6,20 @@ import BeaniesList from './BeaniesList';
 function App() {
   const [beanieBabies, setBeanieBabies] = useState([]);
   const [page, setPage] = useState(1);
-  const perPage = 40;
+  const [search, setSearch] = useState('');
+  const PER_PAGE = 40;
 
   useEffect(() => {
     async function fetch() {
-      const from = page * perPage - perPage;
-      const to = page * perPage;
-      const beanies = await getBeanieBabies(from, to);
+      const start = PER_PAGE * (page - 1) + 1;
+      const end = start + PER_PAGE;
+      const beanies = await getBeanieBabies(start, end, search);
 
       setBeanieBabies(beanies);
     }
 
     fetch();
-  }, [page]); // what can you do with this array to trigger a fetch every time the page changes?
+  }, [page, search]); // what can you do with this array to trigger a fetch every time the page changes?
 
   return (
     <>
@@ -26,11 +27,13 @@ function App() {
       <div className="buttons">
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
+        <h3>Search</h3>
+        <input onChange={(e) => setSearch(e.target.value)} />
         <button onClick={() => setPage(page - 1)} disabled={page === 1}>
           Previous Page
         </button>
         {/* on click, this button should increment the page in state  */}
-        <button onClick={() => setPage(page + 1)} disabled={beanieBabies.length < perPage}>
+        <button onClick={() => setPage(page + 1)} disabled={beanieBabies.length < PER_PAGE}>
           Next Page
         </button>
       </div>
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,21 +1,23 @@
 import { client, checkError } from './client';
 
-export async function getBeanieBabies(from = 0, to = 30) {
-  const response = await client
-    .from('beanie_babies')
-    .select()
-    .range(from, to);
+export async function getBeanieBabies(from = 0, to = 30, search) {
+  if (!search) {
+    const response = await client.from('beanie_babies').select().range(from, to);
 
-  return checkError(response);
+    return checkError(response);
+  } else {
+    const response = await client
+      .from('beanie_babies')
+      .select()
+      .range(from, to)
+      .ilike('title', `%${search}%`);
+
+    return checkError(response);
+  }
 }
 
 export async function getSingleBeanie(id) {
-  const response = await client
-    .from('beanie_babies')
-    .select()
-    .match({ id })
-    .single();
+  const response = await client.from('beanie_babies').select().match({ id }).single();
 
   return checkError(response);
-
 }
